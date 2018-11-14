977da0b26c5973d189c83097d071d91ac007aa45
842Mono <842mono@gmail.com>
2018-11-14 11:52:38 +0200

    T5, T6 and .gitignore

---
+++ Assets/T5/testtoggleanim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testtoggleanim : MonoBehaviour
+{
+	public Animator a;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		a = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			bool t = a.GetBool("that");
+			a.SetBool("that", !t);
+		}
+	}
+
+
+}
---
+++ Assets/T6/animErica.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animErica : MonoBehaviour
+{
+	public Animator thisAnimator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		thisAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			thisAnimator.SetBool("equip", true);
+		}
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			thisAnimator.SetBool("draw", true);
+		}
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			thisAnimator.SetBool("draw", false);
+		}
+
+	}
+}
