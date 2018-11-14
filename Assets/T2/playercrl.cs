a566ee2002cbe866703843f736dc378e996181e4
842Mono <842mono@gmail.com>
2018-11-14 13:04:38 +0200

    some cleanups

---
+++ Assets/T2/playercrl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercrl : MonoBehaviour
+{
+
+	//public GameObject camera;
+	public GameObject tel;
+	public GameObject es;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void FixedUpdate()
+	{
+	  if(Input.GetKey("up"))
+	  {
+        this.transform.Translate(0,0,0.1f);
+		
+        //camera.transform.Translate(0,0,0.1f);
+	  }
+	  if(Input.GetKey("down"))
+	  {
+        this.transform.Translate(0,0,-0.1f);
+		
+        //camera.transform.Translate(0,0,-0.1f);
+	  }
+	  if(Input.GetKey("left"))
+	  {
+        this.transform.Translate(-0.1f,0,0);
+	  }
+	  if(Input.GetKey("right"))
+	  {
+        this.transform.Translate(0.1f,0,0);
+	  }
+	}
+
+	// void OnCollisionEnter(Collision other)
+	// {
+	// 	Debug.Log("test");
+	// }
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Finish")
+			Destroy(this.gameObject);
+		else if(other.tag == "Respawn")
+			this.transform.position = tel.transform.position;
+		else if(other.tag == "done")
+			Destroy(es.gameObject);
+	}
+}
