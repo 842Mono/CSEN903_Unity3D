c494d39d0ff8dc850906274888b7d780a00be37e
842Mono <842mono@gmail.com>
2018-11-19 11:23:54 +0200

    T7

---
+++ Assets/T7/agentCtrl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class agentCtrl : MonoBehaviour
+{
+	public Transform[] points;
+	private int destPoint = 0;
+
+	NavMeshAgent thisNavMeshAgent;
+	NavMeshAgent agent;
+	public GameObject basePoint;
+	public GameObject destination;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		thisNavMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+		//thisNavMeshAgent.SetDestination(destination.transform.position);
+		agent = thisNavMeshAgent;
+		points = new Transform[]{ basePoint.transform, destination.transform };
+
+		// Disabling auto-braking allows for continuous movement
+		// between points (ie, the agent doesn't slow down as it
+		// approaches a destination point).
+		thisNavMeshAgent.autoBraking = false;
+
+		GotoNextPoint();
+		Debug.Log(gameObject.transform.position);
+	}
+
+
+	void GotoNextPoint() {
+		// Returns if no points have been set up
+		if (points.Length == 0)
+			return;
+
+		// Set the agent to go to the currently selected destination.
+		agent.destination = points[destPoint].position;
+		Debug.Log(points[0].transform.position);
+		Debug.Log(points[1].transform.position);
+
+		// Choose the next point in the array as the destination,
+		// cycling to the start if necessary.
+		destPoint = (destPoint + 1) % points.Length;
+	}
+
+
+	void Update () {
+		// Choose the next destination point when the agent gets
+		// close to the current one.
+		if (!agent.pathPending && agent.remainingDistance < 0.5f)
+			GotoNextPoint();
+	}
+}
---
+++ Assets/T7/playerCtrlT7.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCtrlT7 : MonoBehaviour
+{
+	public GameObject destination;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			destination.transform.position = gameObject.transform.position;
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			gameObject.transform.Translate(0.1f, 0, 0);
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			gameObject.transform.Translate(-0.1f, 0, 0);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			gameObject.transform.Translate(0, 0, 0.1f);
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			gameObject.transform.Translate(0, 0, -0.1f);
+		}
+	}
+}
